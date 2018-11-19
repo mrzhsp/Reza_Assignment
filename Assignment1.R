06d4ee8a1928f7c2402da838dbb0600679030cc0
Mohamadreza Hoseinpour <mrz.hsp@gmail.com>
2018-11-19 18:12:08 +0100

    Part 2 - Q3)

--- Assignment1.R
+++ Assignment1.R
@@ -21,6 +21,21 @@ library(readr)
 forbes_raw <- read.csv("forbes.csv")
 forbes_raw$rank <- parse_number(forbes_raw$rank)
 forbes_raw$net_worth <- parse_number(forbes_raw$net_worth)
+forbes_raw$net_worth <- as.numeric(as.character(forbes_raw$net_worth))
 
 # Part 2 - Q2) ------------------------------------------------------------
+# In this question, I have filtered the data by the bellow function.
+# I have notices that there are a number of rows with 3 digit net worth.
+# These can be deleted to prevent distortion in our data.
 forbes_filter <- filter(forbes_raw, net_worth < 950)
+
+# Part 2 - Q3) ------------------------------------------------------------
+ggplot(data = forbes_filter) +
+  geom_point(mapping = aes(x = age, y = net_worth))
+
+# Now the same question with log(net_worth)
+ggplot(data = forbes_filter) +
+  geom_point(mapping = aes(x = age, y = log(net_worth)))
+
+# Considering the log of net_worth, the density of the points is better drawn.
+# ...
