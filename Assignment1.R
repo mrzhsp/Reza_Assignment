16d2a070fb84d686d7aa6bf3030089d0e1283b46
Mohamadreza Hoseinpour <mrz.hsp@gmail.com>
2018-11-19 17:14:12 +0100

    Inserting libraries and Question 2.

--- Assignment1.R
+++ Assignment1.R
@@ -1,1 +1,27 @@
-# Hello
+# Assignment
+
+# Inserting Libraries ----------------------------------------
+library(tidyverse)
+library(tidyr)
+library(tidyselect)
+library(euR)
+library(dplyr)
+library(ggplot2)
+library(GGally)
+library(proto)
+library(sqldf)
+library(gsubfn)
+library(forcats)
+library(microbenchmark)
+library(rmarkdown)
+library(colorspace)
+library(readr)
+
+# Part 2 -----------------------------------------------------
+## Question 1
+forbes_raw <- read.csv("forbes.csv")
+forbes_raw$rank <- parse_number(forbes_raw$rank)
+forbes_raw$net_worth <- parse_number(forbes_raw$net_worth)
+
+## Question 2
+forbes_filter <- filter(forbes_raw, net_worth < 950)
