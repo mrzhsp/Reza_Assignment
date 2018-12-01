e201c1bb0537755b89278e628fb2fefc0528fb62
Mohamadreza Hoseinpour <mrz.hsp@gmail.com>
2018-12-01 12:40:34 +0100

    Uploading files: 1) R script 2) Data

---
+++ assignment-2.R
@@ -0,0 +1,44 @@
+library(tidyverse)
+
+
+# Question 1 ------------------------------------------------------------------------------------------------------
+
+
+tidy_df <- function(data, column_prefix = "var"){
+  
+}
+
+
+# Question 2 ------------------------------------------------------------------------------------------------------
+
+#' Get the Jane Austen data
+#'
+#' It will attempt to install the right package for you. If it does not work,
+#'   try to install it manually.
+#'
+#' @return A data frame with Jane Austen texts, one line per row
+get_jane_austen_data <- function(){
+  
+  tryCatch({library(gutenbergr)}, error = function(e){install.packages("gutenbergr")})
+  library(gutenbergr)
+  
+  austen_text <- gutenberg_works(author == "Austen, Jane") %>% 
+    gutenberg_download(meta_fields = "title") %>% mutate(id = row_number(gutenberg_id))
+  assign("austen_text", austen_text, envir=.GlobalEnv)
+  invisible()
+}
+
+# extract_possible_names 
+
+
+
+
+# Question 3 ------------------------------------------------------------------------------------------------------
+
+# filter_names
+
+
+
+# Question 4 ------------------------------------------------------------------------------------------------------
+
+# count_names_per_book
